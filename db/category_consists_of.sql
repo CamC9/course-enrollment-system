202c7410c88f3ede25126f3217145bd74de6f7f8
Cameron Cuellar <cuellarc660@gmail.com>
2024-05-04 21:04:29 -0700

    Fixed category_consists_of table and added degrees relation

--- db/category_consists_of.sql
+++ db/category_consists_of.sql
@@ -1,27 +1,25 @@
 CREATE TABLE category_consists_of (
+    degree_name VARCHAR(255) NOT NULL,
     category_name VARCHAR(255) NOT NULL,
     course_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (category_name, course_name),
+    PRIMARY KEY (degree_name, category_name, course_name),
     FOREIGN KEY (category_name) REFERENCES category(category_name),
-    FOREIGN KEY (course_name) REFERENCES courses(course_name)
+    FOREIGN KEY (course_name) REFERENCES courses(course_name),
+    FOREIGN KEY (degree_name) REFERENCES degrees(degree_name)
 );
 
 -- Valid category_name and course_name
-INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('Core Courses', 'CSE_132B');
+INSERT INTO category_consists_of (degree_name, category_name, course_name)
+    VALUES ('BSC IN CS', 'Core Courses', 'CSE_132B');
 
--- Invalid category_name
-INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('NOT_A_CATEGORY_NAME', 'CSE_132B');
-
--- Invalid course_name
-INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('Core Courses', 'NOT_A_COURSE_NAME');
+-- Valid category_name and course_name
+INSERT INTO category_consists_of (degree_name, category_name, course_name)
+    VALUES ('BSC IN COGS', 'Core Courses', 'COGS_112');
 
 -- Second course_name for same category_name
-INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('Core Courses', 'CSE_156');
+INSERT INTO category_consists_of (degree_name, category_name, course_name)
+    VALUES ('BSC IN CS', 'Core Courses', 'CSE_156');
 
 -- Second category_name for same course_name
-INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('Electives', 'CSE_132B');
+INSERT INTO category_consists_of (degree_name, category_name, course_name)
+    VALUES ('BSC IN CS', 'Electives', 'CSE_132B');
---
+++ db/degrees.sql
@@ -0,0 +1,15 @@
+CREATE TABLE degrees (
+    degree_name VARCHAR(255) PRIMARY KEY
+);
+
+-- Valid degree name
+INSERT INTO degrees (degree_name)
+    VALUES ('BSC IN CS');
+
+-- Valid degree name
+INSERT INTO degrees (degree_name)
+    VALUES ('BSC IN COGS');
+
+-- Valid degree name
+INSERT INTO degrees (degree_name)
+    VALUES ('BSC IN MATH');
