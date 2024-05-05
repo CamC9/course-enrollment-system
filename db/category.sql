e2622441feef28c209dc1080984df92cac157a59
Cameron Cuellar <cuellarc660@gmail.com>
2024-05-04 20:34:47 -0700

    Fixed category table

--- db/category.sql
+++ db/category.sql
@@ -11,11 +11,3 @@ INSERT INTO category (category_name, min_units, min_gpa)
 -- Second category_name
 INSERT INTO category (category_name, min_units, min_gpa)
     VALUES ('Electives', 12, 3.00);
-
--- Invalid min_units
-INSERT INTO category (category_name, min_units, min_gpa)
-    VALUES ('Electives', -1, 3.00);
-
--- Invalid min_gpa
-INSERT INTO category (category_name, min_units, min_gpa)
-    VALUES ('Electives', 12, 7.00);
