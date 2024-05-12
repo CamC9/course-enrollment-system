9c4815985b112f010fa404b047f5d5848c91597d
Cameron Cuellar <cuellarc660@gmail.com>
2024-05-12 02:25:35 -0700

    Finish teachings form

--- db/teachings.sql
+++ db/teachings.sql
@@ -3,19 +3,19 @@ CREATE TABLE teachings (
     course_name VARCHAR(255) NOT NULL,
     quarter VARCHAR(255) NOT NULL,
     year int NOT NULL,
-    teaching_id int PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     FOREIGN KEY (faculty_name) REFERENCES faculty(name),
     FOREIGN KEY (course_name) REFERENCES courses(course_name)
 );
 
 -- Valid teacher 
-INSERT INTO teachings (faculty_name, course_name, quarter, year, teaching_id) 
-    VALUES ('John Doe', 'CSE_132B', 'SPRING', 2018, 1);
+INSERT INTO teachings (faculty_name, course_name, quarter, year) 
+    VALUES ('John Doe', 'CSE_132B', 'SPRING', 2018);
 
 -- Future quarter
-INSERT INTO teachings (faculty_name, course_name, quarter, year, teaching_id) 
-    VALUES ('Jackson Doe', 'CSE_132B', 'WINTER', 2019, 4);
+INSERT INTO teachings (faculty_name, course_name, quarter, year) 
+    VALUES ('Jackson Doe', 'CSE_132B', 'WINTER', 2019);
 
 -- Past quarter
-INSERT INTO teachings (faculty_name, course_name, quarter, year, teaching_id) 
-    VALUES ('Jane Doe', 'CSE_105', 'FALL', 2017, 5);
+INSERT INTO teachings (faculty_name, course_name, quarter, year) 
+    VALUES ('Jane Doe', 'CSE_105', 'FALL', 2017);
