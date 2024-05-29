9afd807e08aa55f32b1d290be8e654c18866bccc
Cameron Cuellar <cuellarc660@gmail.com>
2024-05-28 21:57:48 -0700

    Add grade conversion relation

---
+++ db/grade_conversion.sql
@@ -0,0 +1,14 @@
+create table grade_conversion (
+    letter_grade CHAR(2) NOT NULL,
+    number_grade DECIMAL(2,1)
+);
+insert into grade_conversion values('A+', 4.3);
+insert into grade_conversion values('A', 4);
+insert into grade_conversion values('A-', 3.7);
+insert into grade_conversion values('B+', 3.4);
+insert into grade_conversion values('B', 3.1);
+insert into grade_conversion values('B-', 2.8);
+insert into grade_conversion values('C+', 2.5);
+insert into grade_conversion values('C', 2.2);
+insert into grade_conversion values('C-', 1.9);
+insert into grade_conversion values('D', 1.6);
