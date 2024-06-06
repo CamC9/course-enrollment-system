5a2ac37797d8466218fc7f0d31b1cf583f55d57f
Cameron Cuellar <cuellarc660@gmail.com>
2024-06-06 15:22:43 -0700

    Bring back category_consists_of on html menu, and reformat relation structure in category_consists_of.sql file

--- db/category_consists_of.sql
+++ db/category_consists_of.sql
@@ -6,10 +6,28 @@ CREATE TABLE category_consists_of (
 );
 
 INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('upper division', 'CSE_132A');
+    VALUES ('upper division', 'CSE132A');
 INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('technical elective', 'CSE_291');
+    VALUES ('technical elective', 'CSE291');
 INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('lower division', 'CSE_101');
+    VALUES ('lower division', 'CSE101');
 INSERT INTO category_consists_of (category_name, course_name)
-    VALUES ('upper division', 'CSE_132B');
+    VALUES ('upper division', 'CSE132B');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('technical elective', 'CSE132B');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('technical elective', 'CSE232A');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'MATH101');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'PHYS101');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'BIO101');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'CHEM101');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'STAT101');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'CSE132C');
+INSERT INTO category_consists_of (category_name, course_name)
+    VALUES ('lower division', 'CSE291B');
